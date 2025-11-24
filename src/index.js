ff3a751cf7add41688332b000e4693eeee3cf57f
dheerendra kumar <testrpa85@gmail.com>
2025-11-24 23:17:41 +0530

    add client url on cors

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ env.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
